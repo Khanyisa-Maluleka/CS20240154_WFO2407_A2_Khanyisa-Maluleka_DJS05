319a58524a335e5e073509e6f01d00cb5565ee78
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-08 22:18:52 +0200

    Created incerement and decrement objects

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let state = 0;
 
+const increment = {type: 'INCREMENT'}
+const increment = {type: 'INCREMENT'}
+
 function getState() {
     return state
 }
 
 console.log(getState())
+
+function increment() {
+    
+}
